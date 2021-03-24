8eac54076c3f45c60029e3591d306126b65a7bea
fabiano nalin <fansoft.br@gmail.com>
2021-03-24 09:59:37 -0300

    UP project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
